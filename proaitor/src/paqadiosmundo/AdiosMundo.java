ff370516b8afc70337493266ad4822755b7df4a6
Aitor <Aitor@DESKTOP-C7H5SE7>
2021-05-24 19:23:47 +0200

    Print Adios Mundo

--- proaitor/src/paqadiosmundo/AdiosMundo.java
+++ proaitor/src/paqadiosmundo/AdiosMundo.java
@@ -1,5 +1,13 @@
 package paqadiosmundo;
 
 public class AdiosMundo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Adio Mundo!");
+		
+		
+	}
 
 }
