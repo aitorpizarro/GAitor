8d2852b9fecaf7e5c41fe08ccf13156ca292f2ee
Aitor <Aitor@DESKTOP-C7H5SE7>
2021-05-24 18:55:38 +0200

    Metodo restar del primer usuario

--- proaitor/src/proaitor/HolaMundo.java
+++ proaitor/src/proaitor/HolaMundo.java
@@ -2,6 +2,10 @@ package proaitor;
 
 public class HolaMundo {
 
+	int restar(int a, int b) {
+		return a=b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
